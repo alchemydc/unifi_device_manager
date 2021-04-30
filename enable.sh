6fa4e11aabdb1d937731eb072f79f11d99810c28
DC <dc@technoexile.com>
2021-04-29 22:26:18 -0600

    . ./ instead of source to activate the venv

--- enable.sh
+++ enable.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 
-source /home/unifi/unifi_device_manager/bin/activate
+. /home/unifi/unifi_device_manager/bin/activate
 cd /home/unifi/unifi_device_manager
 ./unifi_device_manager.py enable
